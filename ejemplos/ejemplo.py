189a7f69309b841f79dd2764631a1ff61f295e3e
alex <alez011@hotmail.com>
2015-11-28 12:06:57 -0600

    Ejemplo

---
+++ ejemplos/ejemplo.py
@@ -0,0 +1,45 @@
+#char cadena
+print "hola"
+print 23
+numero = 98
+cadena = "aasdasda"
+
+print "hola""que tal"
+print 'prueba'
+print 56 * (45 + (32 - 3))
+ca = "hola""mundo"
+print ca
+
+numero = 10
+print numero
+print 44 + 100 * (5 + (numero + 10)-2) + 432
+
+operacion = (54 + 232) * 2
+print operacion
+print ((2 + 2))
+
+cadena2 = cadena
+print cadena2
+
+print 20, numero
+print ca, cadena, 500*423+(54+232),"prueba"+" "+'prueba', 10 + ( 2 * (33+100.11))
+
+ca = 'cadena reemplazada'
+print ca
+
+print "wolf"+cadena
+prueba = cadena2+" "+'ggg'
+print prueba
+
+#char variable
+variable = "esto es una prueba"
+numero = 8782
+
+#float decimal
+decimal = 1442.23
+print decimal
+
+decimal = 32.33
+print 45 + ( decimal * 50) + numero
+print variable, (operacion*(33 / 2) + 100)
+print (1029 + 455) * 2, "otra prueba", 454.23
