b902a5f87f912e7e775dfe8ae4728629d99c341c
msrojas <alez011@hotmail.com>
2015-12-01 17:19:57 -0600

    ejemplo

---
+++ ejemplos/convertir.py
@@ -0,0 +1,10 @@
+x1 = int("90")
+print x1
+print int("9283")
+
+print 5442* 12 + (2312 -(int("32")+345.22))
+
+#float decimal
+decimal = float("1012.13")
+print decimal
+print ((121 *2) + int("987") + decimal)
