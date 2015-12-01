39106e59570931cce0328d545cc8939d56149405
msrojas <alez011@hotmail.com>
2015-12-01 17:18:57 -0600

    Primer upload

---
+++ ejemplos/input.py
@@ -0,0 +1,15 @@
+#raw_input
+cadena_input = raw_input()
+print cadena_input
+
+cadena_input = raw_input("input> ")
+print cadena_input
+
+numero_input = int(raw_input("numero: "))
+print numero_input
+numero_input = int(raw_input("ingresa otro numero: "))
+print numero_input
+
+#float numero_float
+numero_float = float(raw_input("float: "))
+print numero_float
