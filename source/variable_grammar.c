e1547892bbe248f4378528de215fb72fecbb93a3
msrojas <alez011@hotmail.com>
2015-12-08 15:28:45 -0600

    Update: algunos pequeños detalles

--- source/variable_grammar.c
+++ source/variable_grammar.c
@@ -242,6 +242,8 @@ uint16_t get_funciones_extras(lexical * lexer, uint8_t size)
     {
         if(lexer->token == LEN)
             len += 3;
+        else if(lexer->token == INT || lexer->token == FLOAT_F)
+            len += 1;    
         lexer = lexer->next;      
     }
     return len;
