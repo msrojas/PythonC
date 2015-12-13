34b83f9a9d17835c8c68c4caba84659eb53ca47a
msrojas <alez011@hotmail.com>
2015-12-13 14:20:47 -0600

    Update: free agregado y bugs corregidos

--- source/free_lib.c
+++ source/free_lib.c
@@ -20,25 +20,180 @@
 */    
 #include "free_lib.h"
 
-void libera_free_vars()
+free_table_t * free_create(uint32_t size)
 {
-    if(size_free == 0 && free_variables != NULL)
+	free_hash = (free_table_t *)malloc(sizeof(free_table_t));
+	if(free_hash == NULL)
+		return NULL;
+
+	if((free_hash->table = malloc(size*sizeof(free_elem_t*))) == NULL)
+	{
+		free(free_hash);
+		return NULL;
+	}
+
+	free_hash->size = size;
+
+	uint32_t i = 0;
+	for(i=0;i<size;i++)
+		free_hash->table[i] = NULL;
+}
+
+uint32_t free_calc_hash(uint8_t * key)
+{
+	uint32_t h = 5381;
+	while(*(key++))
+		h = ((h << 5) + h) + (*key);
+
+	return h;
+}
+
+uint8_t free_libera(uint8_t * key,uint8_t liberar)
+{
+    uint32_t h = free_calc_hash(key) % free_hash->size;
+    free_elem_t * e = free_hash->table[h];
+
+    while(e != NULL)
     {
-        free(free_variables);
-        return;
+        if(strcmp(e->var_name, key) == 0)
+        {
+            e->liberada = liberar;
+            return 1;
+        }
+        e = e->next;
     }
 
-    uint8_t i=0;
-    free_struct * temp;
+    debug("\nError: la variable no existe: %s\n", key);
 
-    for(i=0;i<=size_free;i++)
-    {
-        temp = free_variables;
-        free_variables = free_variables->next;
-        if(temp->var_name != NULL)
-            free(temp->var_name);
-        free(temp);
-    }
+    return 0;
+}
+
+uint8_t free_put(uint8_t * key, uint8_t liberar)
+{
+	uint32_t h = free_calc_hash(key) % free_hash->size;
+	free_elem_t * e = free_hash->table[h];
+
+	while(e != NULL) //Para evitar colisiones
+	{
+		if(strcmp(e->var_name, key) == 0)
+		{
+			printf("Error: posible bug\n"); //La variable solo debe ser almacenada una vez
+			return 2;
+		}
+
+		e = e->next; 
+	}
+
+	if((e = malloc(sizeof(free_elem_t))) == NULL)
+	{
+		printf("Error: ocurrio un inconveniente en la heap\n");
+		return 0;
+	}
+
+	e->var_name = (uint8_t *)malloc(strlen(key)+1);
+	if(e->var_name == NULL)
+	{
+		printf("Error: ocurrio un inconveniente en la heap\n");
+		return 0;
+	}
+
+	strcpy(e->var_name, key);
+	e->liberada = liberar;
+
+	e->next = free_hash->table[h];
+	free_hash->table[h] = e;
+
+	return 1;
+}
+
+void * free_remove(uint8_t * key)
+{
+	uint32_t h = free_calc_hash(key) % free_hash->size;
+	free_elem_t * e = free_hash->table[h];
+	free_elem_t * prev = NULL;
+
+	while(e != NULL)
+	{
+		if(strcmp(e->var_name, key) == 0)
+		{
+			void * data = e->var_name;
+
+			if(prev != NULL)
+				prev->next = e->next;
+			else
+				free_hash->table[h] = e->next;
+			free(e);
+			e = NULL;
+			return data;
+		}
+		prev = e;
+		e = e->next;
+	}
+
+	return 0;
+
+}
+
+uint8_t free_get(uint8_t * key)
+{
+	uint32_t h = free_calc_hash(key) % free_hash->size;
+	free_elem_t * e = free_hash->table[h];
+
+	while(e != NULL)
+	{
+		if(strcmp(e->var_name, key) == 0)
+		{
+			return e->liberada;
+		}
+
+		e = e->next;
+	}
+
+	return 0;
+}
+
+free_elem_t * free_iterate(free_elem_it * iterator)
+{
+	while(iterator->elem == NULL)
+	{
+		if(iterator->index < iterator->ht->size - 1)
+		{
+			iterator->index++;
+			iterator->elem = iterator->ht->table[iterator->index];
+		}
+		else
+			return NULL;
+	}
+
+	free_elem_t * e = iterator->elem;
+	if(e)
+		iterator->elem = e->next;
+	return e;
+}
+
+uint8_t * free_iterate_keys(free_elem_it * iterator)
+{
+	free_elem_t * e = free_iterate(iterator);
+	return(e == NULL ? NULL : e->var_name);
+}
+
+void free_clear(int free_data)
+{
+	free_elem_it it = FREE_ITERATOR(free_hash);
+	uint8_t * k = free_iterate_keys(&it);
+
+	while(k != NULL)
+	{
+		free_data ? free(free_remove(k)) : free_remove(k);
+		k = free_iterate_keys(&it);
+	}
+}
+
+void free_destroy()
+{
+	free_clear(1);
+	free(free_hash->table);
+	free(free_hash);
 }
 
 void free_vars_compile(FILE * archivo)
@@ -47,54 +202,16 @@ void free_vars_compile(FILE * archivo)
 	uint8_t temp[100];
 	memset(temp, 0, sizeof(temp));
 
-	free_variables = free_variables->next; //Omitimos el nodo 0 (que es el root)
-
-	for(i=0;i<size_free;i++)
+	free_elem_it it = FREE_ITERATOR(free_hash);
+	free_elem_t * e = free_iterate(&it);
+	while(e != NULL)
 	{
-		sprintf(temp, "free(%s);", free_variables->var_name);
-		fprintf(archivo, "%*s\n", strlen(temp)+4,temp);
-		memset(temp, 0, sizeof(temp));
-		free_variables = free_variables->next;
+		if(e->liberada == 1)
+		{
+                    sprintf(temp, "free(%s);", e->var_name);
+		    fprintf(archivo, "%*s\n", strlen(temp)+4,temp);
+		    memset(temp, 0, sizeof(temp));
+		}
+		e = free_iterate(&it);
 	}
-}
-
-void acomodar_free_vars()
-{
-	free_variables->next = free_root;
-	free_variables = free_root;
-}
-
-uint8_t agregar_free_var(uint8_t * var)
-{
-	free_struct * new_node = (free_struct *)malloc(sizeof(free_struct));
-	if(new_node == NULL)
-	{
-		debug("\nError: ocurrio un inconveniente en la heap\n");
-		return 0;
-	}
-
-	new_node->var_name = (uint8_t *)malloc(strlen(var)+1);
-	if(new_node->var_name == NULL)
-	{
-		debug("\nError: ocurrio un inconveniente en la heap\n");
-		return 0;
-	}
-
-	strcpy(new_node->var_name, var);
-	free_variables->next = new_node;
-	free_variables = new_node;
-
-	return 1;
-}
-
-uint8_t init_free()
-{
-	free_variables = (free_struct *)malloc(sizeof(free_struct));
-	if(free_variables == NULL)
-	{
-		debug("\nError: ocurrio un inconveniente en la heap\n");
-		return 0;
-	}
-
-	return 1;
 }
