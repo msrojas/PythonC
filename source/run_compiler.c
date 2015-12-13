d0a4ae665c87ed460f6e5a4a1497b275457508a7
msrojas <alez011@hotmail.com>
2015-12-13 14:20:21 -0600

    Update: free agregado y bugs corregidos

--- source/run_compiler.c
+++ source/run_compiler.c
@@ -23,9 +23,7 @@
 #include <unistd.h>
 
 hashtable_t * hash = NULL;
-free_struct * free_variables = NULL;
-free_struct * free_root = NULL;
-uint32_t size_free = 0;
+free_table_t * free_hash = NULL;
 uint32_t linea = 1;
 uint8_t fixed_width = 0;
 var_definidas * definir = NULL;
@@ -54,16 +52,15 @@ void agregar_info_final(FILE * archivo)
         uint8_t temp_indentacion = indentacion;
         for(i=0;i<indentacion;i++)
         {
-            fprintf(archivo, "%*s\n", 4*temp_indentacion+1, "}");
-            temp_indentacion--;
+        	fprintf(archivo, "%*s\n", 4*temp_indentacion+1, "}");
+        	temp_indentacion--;
         }
     }
-	
-    if(free_variables != NULL)
+
+    if(free_hash != NULL)
     {   
-        acomodar_free_vars();
         free_vars_compile(archivo);
-        libera_free_vars();
+        free_destroy();
     }
 
     fprintf(archivo, "\n%*s\n", strlen("return 0;")+4,"return 0;");
@@ -81,7 +78,7 @@ void print_output_in_archivo(FILE * archivo, output_code * codigo)
     for(i=0;i<len;i++)
     {
         if(codigo->cadena_retorno[i] == '\n')
-        {
+        {   
             if(temp[0] == '}' && codigo->temp_indentacion > 0)
             {
                 fprintf(archivo, "%*s\n", codigo->temp_indentacion*4+1, "}");
@@ -120,7 +117,7 @@ symbol_table_sizes * get_size_file(FILE * archivo_output)
         else 
             indice++;
     }
-    
+
     if(indice > 0 && linea_l == 0)
         linea_l++;
 
@@ -146,6 +143,7 @@ uint8_t informacion_basica(FILE * archivo_output, FILE * archivo)
     fprintf(archivo_output, "#include <string.h>\n");
     if(fixed_width == 1)
         fprintf(archivo_output, "#include <inttypes.h>\n");
+    print_strchar(archivo_output);
     if(!buscar_funciones(archivo, archivo_output))
     	return 0;
     fprintf(archivo_output, "\nint main()\n{\n");
@@ -195,6 +193,117 @@ uint8_t check_ext_name(char * name)
         debug("[-] El nombre no tiene extension .py\n");
         return 0;
     }
+}
+
+uint16_t get_espacios(uint8_t * cadena)
+{
+    uint16_t len = strlen(cadena);
+    uint16_t espacios = 0;
+    uint16_t i = 0;
+
+    for(i=0;i<len;i++)
+    {
+        if(isspace(cadena[i]))
+            espacios += 4;
+        else if(cadena[i] == '\t')
+            espacios += 4;
+        else
+        {
+            if(espacios != 0 && !check_indentacion(espacios))
+                return 0;
+            if(espacios == 0)
+                espacios = 4;
+            else
+                espacios += 4;
+            return espacios;
+        }
+    }
+}
+
+datos_comentarios * init_datos_comentarios(uint16_t len, uint8_t token) //NUEVO
+{
+    datos_comentarios * datos = malloc(sizeof(datos_comentarios));
+    if(datos == NULL)
+    {
+    	debug("\nError: oucrrio un inconveniente en la heap\n");
+    	return NULL;
+    }
+
+    datos->index = len;
+    datos->token = token;
+    return datos;
+}
+
+datos_comentarios * comentario_match(uint8_t * cadena) //NUEVO
+{
+    uint16_t len = strlen(cadena);
+    uint8_t temp[len+1];
+    memset(temp, 0, sizeof(temp));
+
+    uint16_t i=0;
+    uint16_t indice = 0; //NUEVO
+    uint8_t chars = 0;
+    for(i=0;i<len;i++)
+    {
+        if(isspace(cadena[i]) || cadena[i] == '\t' || cadena[i] == '(') //NUEVO
+        {
+            if((isspace(cadena[i]) || cadena[i] == '\t') && chars == 1)
+                break;
+            else if(cadena[i] == '(')
+                break;
+        }
+        else
+        {
+            if(chars == 0)
+                chars++;
+            temp[indice++] = cadena[i];
+        }
+    }
+
+    datos_comentarios * datos = NULL;
+
+    if(strcmp(temp, "#char") == 0)
+    {
+        datos = init_datos_comentarios(i, CHAR);
+        if(datos == NULL)
+            return NULL;
+    }
+    else if(strcmp(temp, "#float") == 0)
+    {
+        datos = init_datos_comentarios(i, FLOAT);
+        if(datos == NULL)
+            return NULL;
+    }
+    else if(strcmp(temp, "#free") == 0) //NUEVO
+    {
+        datos = init_datos_comentarios(i, FREE);
+        if(datos == NULL)
+            return NULL;
+    }
+
+    return datos;
+}
+
+uint8_t busca_comentario(uint8_t * comentario) //NUEVO
+{
+    uint16_t len = strlen(comentario);
+    uint16_t i = 0;
+    uint8_t chars = 0;
+    for(i=0;i<len;i++)
+    {
+        if(isspace(comentario[i]) || comentario[i] == '\t')
+        {
+            //if(chars)
+        }
+        else if(comentario[i] == '#')
+        {
+            if(chars == 0)
+                return 1;
+        }
+        else
+        	return 0;
+    }
+    return 0;
 }
 
 int main(int argc, char *argv[])
@@ -298,6 +407,7 @@ int main(int argc, char *argv[])
     }
 
     ht_create(symbol->symbol_variable);
+    free_create(symbol->symbol_variable);
     if(symbol->symbol_comentario_var > 0)
         init_definir(symbol->symbol_comentario_var);
 
@@ -308,12 +418,10 @@ int main(int argc, char *argv[])
         fclose(archivo_output);
         free(symbol);
         ht_destroy();
-        if(definir != NULL)
-            var_destroy();
         if(ret == 1)
             error_delete_file(argv[2]);
         else
-            error_delete_file(argv[1]);
+           	error_delete_file(argv[1]);
         exit(1);
     }
 
@@ -330,19 +438,76 @@ int main(int argc, char *argv[])
         if(len > 1 && cadena[len-1] == '\n')
             cadena[len-1] = '\0';
 
-        if(cadena[0] == '#')
+        if(cadena[0] == '#' || busca_comentario(cadena)) //NUEVO
         {
-            if(!verificar_comentario(cadena, symbol->symbol_comentario_var))
-           {
-                fclose(archivo_output);
-                fclose(archivo);
+        	datos_comentarios * datos = comentario_match(cadena);
+            if(datos != NULL && (datos->token == CHAR || datos->token == FLOAT))
+            {
+                if(!verificar_comentario(cadena, datos))
+                {
+                    fclose(archivo_output);
+                    fclose(archivo);
+                    liberacion_general();
+                    free(symbol);
 
-                if(ret == 1)
-                    error_delete_file(argv[2]);
-                else
-                    error_delete_file(argv[1]);
-                exit(1);
+                    if(ret == 1)
+                        error_delete_file(argv[2]);
+                    else
+                        error_delete_file(argv[1]);
+                    exit(1);
+                }
             }
+            else if(datos != NULL && datos->token == FREE)
+            {
+                uint8_t * retorna = comentario_free(cadena, datos->index);
+                if(retorna == NULL)
+                {
+                    fclose(archivo_output);
+                    fclose(archivo);
+                    liberacion_general();
+                    free(symbol);
+
+                    if(ret == 1)
+                        error_delete_file(argv[2]);
+                    else
+                        error_delete_file(argv[1]);
+                    exit(1);
+                }
+                uint16_t espacios = get_espacios(cadena);
+                if(espacios == 0)
+                {
+                    fclose(archivo_output);
+                    fclose(archivo);
+                    liberacion_general();
+                    free(symbol);
+
+                    if(ret == 1)
+                        error_delete_file(argv[2]);
+                    else
+                        error_delete_file(argv[1]);
+                    exit(1);
+                }
+                espacios -= 4;
+                output_code * codigo_temp = get_output(retorna, espacios);
+                if(codigo_temp == NULL)
+                {
+                    fclose(archivo_output);
+                    fclose(archivo);
+                    free(symbol);
+
+                    if(ret == 1)
+                        error_delete_file(argv[2]);
+                    else
+                        error_delete_file(argv[1]);
+                    exit(1);
+                }
+                print_output_in_archivo(archivo_output, codigo_temp);
+                free(codigo_temp->cadena_retorno);
+                free(codigo_temp);
+                free(retorna);
+            }
+            if(datos != NULL)
+                free(datos);
             memset(cadena, 0, sizeof(cadena));
             linea++;
             continue;
@@ -367,6 +532,7 @@ int main(int argc, char *argv[])
                 error_delete_file(argv[1]);
             exit(1);
         }
+        //printf("%s\n", codigo->cadena_retorno);
         print_output_in_archivo(archivo_output, codigo);
         free(codigo->cadena_retorno);
         free(codigo);
