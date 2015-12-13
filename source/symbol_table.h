0bb67484b972083c9dfff5b10a1f0dffda5e6191
msrojas <alez011@hotmail.com>
2015-12-13 14:20:17 -0600

    Update: bugs corregidos y nuevos funciones

--- source/symbol_table.h
+++ source/symbol_table.h
@@ -8,6 +8,8 @@ typedef struct hash_elem_t
 	struct hash_elem_t * next;
 	uint8_t * var_name;
 	uint8_t token;
+	uint8_t token_function;
+	uint8_t * value; 
 }hash_elem_t;
 
 typedef struct hashtable_t
@@ -30,11 +32,18 @@ extern hashtable_t * hash;
 void print_token(uint8_t token);
 hashtable_t * ht_create(unsigned int size);
 unsigned int ht_calc_hash(uint8_t * key);
-uint8_t ht_put(uint8_t * key, uint8_t token);
+uint8_t ht_put(uint8_t * key, uint8_t token, uint8_t token_function,uint8_t * value);
 void * ht_remove(uint8_t * key);
+uint16_t ht_get_len(uint8_t * key);
 uint8_t ht_get(uint8_t * key);
 hash_elem_t * ht_iterate(hash_elem_it * iterator);
 uint8_t * ht_iterate_keys(hash_elem_it * iterator);
 void ht_destroy();
+uint8_t * ht_get_value(uint8_t * key);
+uint16_t get_len_value(lexical * lexer, uint8_t size);
+uint8_t * agregar_valor(lexical * lexer, uint8_t size);
+uint8_t update_valor(uint8_t * key, uint8_t * value);
+uint8_t update_token_function(uint8_t * key, uint8_t token_function);
+
 
 #endif
