91e38673cea2b669bf45ea623ac37e83315f0758
msrojas <alez011@hotmail.com>
2015-12-19 12:36:28 -0600

    Create raw_input.h

---
+++ source/include/raw_input.h
@@ -0,0 +1,11 @@
+#ifndef RAW_INPUT_H
+#define RAW_INPUT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+char * raw_input(char * input);
+int raw_input_int(char * input);
+float raw_input_float(char * input);
+
+#endif
